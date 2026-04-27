b39b217ef8ce0d2765cc2d8ce8482ddccad63096
Clawbot <clawbot@xianghus-mini.dyn.ucr.edu>
2026-04-27 10:13:22 -0700

    fix: Add MIME types for SVG and PNG so young crops render correctly

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const types = {
   ".html": "text/html; charset=utf-8",
   ".css": "text/css; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
-  ".json": "application/json; charset=utf-8"
+  ".json": "application/json; charset=utf-8",
+  ".svg": "image/svg+xml",
+  ".png": "image/png"
 };
 
 const server = http.createServer((req, res) => {
